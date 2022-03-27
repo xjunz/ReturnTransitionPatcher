1d786f2aa430dc893b85f21deb7f12587038a881
xjunz <zxj19994@gmail.com>
2022-03-28 02:46:09 +0800

    V1.1.0
    - Refine the patch timing
    - Only patch every activity instance once
    - Ignore patching when an activity is finishing

--- ReturnTransitionPatcher/src/main/java/top/xjunz/returntransitionpatcher/ReturnTransitionPatcher.kt
+++ ReturnTransitionPatcher/src/main/java/top/xjunz/returntransitionpatcher/ReturnTransitionPatcher.kt
@@ -5,19 +5,21 @@ import android.app.Application
 import android.app.Instrumentation
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import top.xjunz.returntransitionpatcher.ReturnTransitionPatcher.patchActivity
 import top.xjunz.returntransitionpatcher.ReturnTransitionPatcher.patchAll
 
 /**
  * A patcher to fix the issue on Android 10 and above that after an [Activity] going through
- * [onStop][Activity.onStop], its (shared element) return transition is gone. You can simply call
- * [patchAll] in [Application.onCreate] to patch all activities or call [patchActivity] in
- * [Activity.onPause] (or anywhere else after [Activity.onStart] and before [Activity.onStop])
- * to patch a specific [Activity].
+ * [onStop][Activity.onStop], its (shared element) return transition is gone.
  *
- * The patcher supports up to API 31.
+ * You can simply call [patchAll] to patch all activities or call [patchActivity] to patch a
+ * specific [Activity].
+ *
  */
 object ReturnTransitionPatcher {
+
+    private val TAG_PATCH_STATE = "xjunz.ReturnTransitionPatcher.PATCHED".hashCode()
 
     /**
      * Call this to patch all activities in the [application].
@@ -25,30 +27,37 @@ object ReturnTransitionPatcher {
     fun patchAll(application: Application) {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) return
         application.registerActivityLifecycleCallbacks(object :
-            Application.ActivityLifecycleCallbacks {
-            override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {}
+            ActivityLifecycleCallbacksAdapter {
 
-            override fun onActivityStarted(activity: Activity) {}
-
-            override fun onActivityResumed(activity: Activity) {}
-
-            override fun onActivityPaused(activity: Activity) {
-                patchActivity(activity)
+            override fun onActivityPreStopped(activity: Activity) {
+                doPatch(activity)
             }
-
-            override fun onActivityStopped(activity: Activity) {}
-
-            override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {}
-
-            override fun onActivityDestroyed(activity: Activity) {}
         })
     }
 
+    private fun doPatch(activity: Activity) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) return
+        // When the [onActivityPreStopped] is called because of finishing, ignore it.
+        if (activity.isFinishing) return
+        // Store a patch state in the decor view's tag to avoid repeated patching.
+        val decor = activity.window.peekDecorView()
+        if (decor.getTag(TAG_PATCH_STATE) != true) {
+            Instrumentation().callActivityOnSaveInstanceState(activity, Bundle())
+            decor.setTag(TAG_PATCH_STATE, true)
+            Log.i("ReturnTransitionPatcher", "${activity.javaClass.simpleName} is patched.")
+        }
+    }
+
     /**
-     * Patch a specific [activity].
+     * Patch a specific [activity]. You may call this anytime before the first possible [Activity.onStop],
+     * such as in [Activity.onCreate].
      */
     fun patchActivity(activity: Activity) {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) return
-        Instrumentation().callActivityOnSaveInstanceState(activity, Bundle())
+        activity.registerActivityLifecycleCallbacks(object : ActivityLifecycleCallbacksAdapter {
+            override fun onActivityPreStopped(activity: Activity) {
+                doPatch(activity)
+            }
+        })
     }
 }
